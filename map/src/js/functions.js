e85c204675d498018ca26c6d2d0ee12469dde0d0
jonlee from archPi3 <jonlee836@gmail.com>
2017-07-17 10:34:34 -0700

    'finished menu'

--- map/src/js/functions.js
+++ map/src/js/functions.js
@@ -1,19 +1,6 @@
-/*
-  function initMap() {
-  var uluru = {lat: 48.709657, lng: 44.512527};
-  var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 10,
-  center: uluru
-  
-  });
-  
-  var marker = new google.maps.Marker({
-  position: uluru,
-  map: map
-  });
+var navWidth="200px";
+var navOnOff=false;
 
-  }
-*/
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
 	center: {lat: 48.6809657, lng: 44.512527},
@@ -23,9 +10,28 @@ function initMap() {
 }
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("mySidenav").style.width = navWidth;
 }
 
+
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0px";
 }
+
+window.onclick = function(event) {     
+    if(	!event.target.matches(".button") &&
+	!event.target.matches(".sidenav") &&
+	!event.target.matches("a") &&
+	!event.target.matches(".toggle-button-sidenav") &&
+	document.getElementById("mySidenav").style.width == navWidth){
+	
+	closeNav();
+    }
+}
+
+window.onload = function(){
+    alert
+}
+    // if (!event.target.matches(".sidenav") && document.getElementById("mySidenav").style.width == navWidth){
+    // 	alert("clicked outside")
+    // }
